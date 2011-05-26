2fe15f536f9dfeb222c1e459b439451ab5ed09cd
Prashant <tibrewalprashant1@gmail.com>
2011-05-26 12:47:36 +0530

    modifying

--- src/sin.rb
+++ src/sin.rb
@@ -8,14 +8,14 @@ DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV["DATABASE_URL"]||"sqlite3://#{(Dir.pwd).chomp("src")}db/tweets.db")
 
 get '/' do   
-   haml :"../../views/tweeter"
+   haml :tweeter
 end
 
 get '/fetching_tweets' do
    screen = params[:screen_name]
    if screen.empty?
       @err = "ERROR! Screen name cannot be blank"
-      haml :"../../views/err"
+      haml :err
    else
       DataMapper.finalize
       DataMapper.auto_migrate!
@@ -24,14 +24,14 @@ get '/fetching_tweets' do
       if @err.nil?
          redirect '/search'
       else
-         haml :"../../views/err"
+         haml :err
       end
    end
 end
 
 get '/search' do
    @tweets_all = Tweet.all(:order => [:created_at.desc])
-   haml :"../../views/tweeter_search"
+   haml :err
 end
 
 @search = nil
@@ -66,5 +66,5 @@ get '/search/searching' do
    else
       @err = "ERROR! Type not selected."
    end
-   haml :"../../views/show_result"
+   haml :err
 end
